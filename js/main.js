7aaf4ea35e3f175ed3e2274fc30d1ccee98237da
Roman Gulevskiy <roman.gulevskiy@gmail.com>
2021-06-20 19:32:42 +0500

    Create: booking modal

--- js/main.js
+++ js/main.js
@@ -1,26 +1,44 @@
-const hotelSlider = new Swiper(".hotel-slider", {
-  // Optional parameters
-  loop: true,
+$(document).ready(function () {
+  const hotelSlider = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
-  },
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
+  });
 
-const reviewsSlider = new Swiper(".reviews-slider", {
-  // Optional parameters
-  loop: true,
+  const reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+  });
 
-let menuButton = document.querySelector(".header-top__menu-button");
-menuButton.addEventListener("click", function () {
-  document.querySelector(".header-nav").classList.toggle("header-nav--visible");
+  let menuButton = $(".header-top__menu-button");
+  menuButton.on("click", function () {
+    $(".header-nav").toggleClass("header-nav--visible");
+  });
+
+  let modalButton = $("[data-toggle=mobile]");
+  let closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    $(".modal__overlay").addClass("modal__overlay--visible");
+    $(".modal__dialogue").addClass("modal__dialogue--visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    $(".modal__overlay").removeClass("modal__overlay--visible");
+    $(".modal__dialogue").removeClass("modal__dialogue--visible");
+  }
 });
